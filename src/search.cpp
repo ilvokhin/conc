f8dd97cf9179e2e0be5c32f9dfb8a47b26ead01c
r3t <r3tsky@gmail.com>
2013-04-28 21:27:17 +0400

    BSBI_Search::collect impl

--- src/search.cpp
+++ src/search.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <exception> //TODO
+#include <iostream>
 #include <iterator>
 #include <utility>
 #include <fstream>
@@ -9,6 +10,7 @@
 
 namespace conc
 {
+
 BSBI_Search::BSBI_Search(const std::string& main)
 {
 	std::ifstream conf( main.c_str() );
@@ -27,7 +29,35 @@ BSBI_Search::BSBI_Search(const std::string& main)
 	
 }
 
-void BSBI_Search::find(std::vector<std::string>& q)
-{ }
+void BSBI_Search::find(const std::vector<std::string>& q)
+{ 
+	std::vector<std::vector<Term> > v = collect(q);
+	std::vector<Term>::iterator it = v[0].begin();
+	std::vector<Term> res;
+	// TODO: write this
+	std::cout << "Result:" << std::endl;
+	std::copy(res.begin(), res.end(), std::ostream_iterator<Term>(std::cout, "\n"));
+}
+
+std::vector<std::vector<Term> > BSBI_Search::collect(const std::vector<std::string>& q)
+{
+	std::vector<std::vector<Term> > v( q.size() );
+	std::ifstream idx( index.c_str() );
+	for(int i = 0; i < (int) q.size(); i++)
+	{
+		std::map<std::string, int>::iterator f = word2index_pos.find(q[i]);
+		if( f == word2index_pos.end() ) return std::vector<std::vector<Term> >();
+		idx.seekg(f->second);
+		int input[3];
+		idx.read(reinterpret_cast<char*>(input), sizeof(input) );
+		int word_num = input[0];
+		while( word_num == input[0] )
+		{
+			idx.read(reinterpret_cast<char*>(input), sizeof(input) );
+			v[i].push_back( Term(input[0], input[1], input[2]) );
+		}
+	}
+	return v;
+}
 
 }
--- src/search.h
+++ src/search.h
@@ -1,6 +1,8 @@
 #ifndef __search_h__
 #define __search_h__
 
+#include "helpers.h"
+
 #include <vector>
 #include <string>
 #include <map>
@@ -13,22 +15,24 @@ class Search
 protected:
 	std::vector<std::string> files;
 public:
-	virtual void find(std::vector<std::string>&) = 0;
+	virtual void find(const std::vector<std::string>&) = 0;
 };
 
 class BSBI_Search: public Search
 {
 	std::string index, word2index_pos_filename;
 	std::map<std::string, int> word2index_pos;
+	
+	std::vector<std::vector<Term> > collect(const std::vector<std::string>&);
 public:
 	BSBI_Search(const std::string&);
-	virtual void find(std::vector<std::string>&);
+	virtual void find(const std::vector<std::string>&);
 };
 
 class SPIMI_Search: public Search
 {
 public:
-	virtual void find(std::vector<std::string>&);
+	virtual void find(const std::vector<std::string>&);
 };
 
 }
