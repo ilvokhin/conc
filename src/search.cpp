1a01e0530bc317f3399b55ef1ad7bb532c20f52a
r3t <r3tsky@gmail.com>
2013-05-28 16:45:17 +0400

    fix bug with extra word

--- src/search.cpp
+++ src/search.cpp
@@ -14,18 +14,18 @@ namespace conc
 BSBI_Search::BSBI_Search(const std::string& main)
 {
 	std::ifstream conf( main.c_str() );
-	if( !conf ) throw std::exception(); // TODO: change it
+	if( !conf ) err(main), err(), throw std::exception(); // TODO: change it
 	std::string files_filename;
 	conf >> index >> word2index_pos_filename >> files_filename;
 	std::ifstream w2ip( word2index_pos_filename.c_str() );
-	if( !w2ip ) throw std::exception();
+	if( !w2ip ) err(word2index_pos_filename), err(), throw std::exception();
 	std::string s;
 	int pos;
 	while( w2ip >> s >> pos )
 		word2index_pos.insert(word2index_pos.end(), make_pair(s, pos));
 	std::ifstream file_lst( files_filename.c_str() );
 	// can be improve
-	if( !file_lst ) throw std::exception();
+	if( !file_lst ) err(files_filename), err(), throw std::exception();
 	std::copy(std::istream_iterator<std::string>(file_lst), std::istream_iterator<std::string>(), std::back_inserter(files));
 	
 }
@@ -49,11 +49,13 @@ std::vector<std::pair<Term, Term> > BSBI_Search::find(std::vector<std::string>& 
 			f = std::lower_bound(v[i].begin(), v[i].end(), Term(v[i].begin()->word, it->file, pos));
 			if(f == v[i].end() ||
 				(*f != Term(v[i].begin()->word, it->file, pos + 1 ) && *f != Term(v[i].begin()->word, it->file, pos)))
+				//&& *f != Term(v[i].begin()->word, it->file, pos + 2))) // FIXME: dirty trick
 			{ 
 				found = false;  
 				break; 
 			}
 			else if( *f == Term(v[i].begin()->word, it->file, pos + 1) ) pos++;
+			//else if( *f == Term(v[i].begin()->word, it->file, pos + 2) ) pos += 2; // case of double punctuation
 		}
 		if( found )
 		{
@@ -62,9 +64,9 @@ std::vector<std::pair<Term, Term> > BSBI_Search::find(std::vector<std::string>& 
 			res.push_back(std::make_pair(begin, end));
 		}
 	}
-	/*
+	
 	// TODO: remove
-	std::cout << "Result:" << std::endl;
+	/*std::cout << "Result:" << std::endl;
 	if( !res.empty() )
 	{
 		for(std::vector<std::pair<Term, Term> >::iterator it = res.begin(); it != res.end(); it++)
